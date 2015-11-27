52ef71407de74efd4dd4ea40d454c71f4ce0e437
s13t275 <dik.piyo.piyo@gmail.com>
2015-11-27 13:32:49 +0900

    fix rand wall

--- Dron.java
+++ Dron.java
@@ -18,17 +18,15 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 	private int block;
 	private int xL, yL, xR, yR;
 	private int dxL, dyL, dxR, dyR;
-	private boolean liveL, liveR;
-	private int countL, countR;
+	private boolean liveL, liveR, liveA;
 	private Thread thread;
 	private String message;
 	private Font font;
 	private int bKeyL = 'D', bKeyR = 'I';	// １つ前に押したキー(最初の進行方向で初期化)
 
+	// rand wall
 	private int xA,yA;
 	private int dxA, dyA;
-	private int turn_a = 1;
-	private int turn_b = 1;
 	private int num[] = {-1,1,0};
 
 	private Image img;     // オフスクリーンイメージ
@@ -59,7 +57,7 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 		xA = yA = 40;
 		dxL = dxR = 0;
 		dyL = 1; dyR = -1;
-		liveL = liveR = true;
+		liveL = liveR = liveA = true;
 	}
 
 	@Override
@@ -103,22 +101,12 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 			offg.setFont(font);
 			offg.setColor(Color.BLACK);
 			offg.drawString(title, block*(xSize/3), block*(ySize/3));
-
 			message = "Press the Enter";
 			font = new Font("Monospaced", Font.BOLD, 20);
 			offg.setFont(font);
 			offg.setColor(Color.RED);
 			offg.drawString(message, block*(xSize/40*9), block*(ySize/4*3));
-
-			font = new Font("Monospaced", Font.PLAIN, 12);
-			offg.setFont(font);
-			offg.setColor(Color.RED.darker());
-			offg.drawString("Left:  S(←), D(↓), E(↑), F(→)", 2*block, block*(ySize+6));
-			offg.setColor(Color.BLUE.darker());
-			offg.drawString("Right: J(←), K(↓), I(↑), L(→)", 2*block, block*(ySize+9));
 		} else if ( g_state == 1 ) {
-			message = "Game started!";
-			font = new Font("Monospaced", Font.PLAIN, 12);
 			// 一旦、別の画像（オフスクリーン）に書き込む
 			int i, j;
 			for (i=0; i<xSize; i++) {
@@ -127,19 +115,12 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 					offg.fillRect(i*block, j*block, block, block);
 				}
 			}
-			offg.setFont(font);
-			offg.setColor(Color.GREEN.darker());
-			offg.drawString(message, 2*block, block*(ySize+3));
-			offg.setColor(Color.RED.darker());
-			offg.drawString("Left:  S(←), D(↓), E(↑), F(→)", 2*block, block*(ySize+6));
-			offg.setColor(Color.BLUE.darker());
-			offg.drawString("Right: J(←), K(↓), I(↑), L(→)", 2*block, block*(ySize+9));
 		} else {
 			// 結果の表示
 			switch ( g_state ) {
-			case 2: message = "Draw!"; offg.setColor(Color.YELLOW.darker()); break;
-			case 3: message = "R won!"; offg.setColor(Color.BLUE.darker()); break;
-			case 4: message = "L won!"; offg.setColor(Color.RED.darker()); break;
+			case 2: message = "Draw!"; offg.setColor(Color.YELLOW.darker()); break;    // 引分
+			case 3: message = "R won!"; offg.setColor(Color.BLUE.darker()); break;     // R勝利
+			case 4: message = "L won!"; offg.setColor(Color.RED.darker()); break;      // L勝利
 			}
 			font = new Font("Monospaced", Font.BOLD, 30);
 			offg.setFont(font);
@@ -159,37 +140,32 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 			message = "No : Close the Window";
 			offg.setColor(Color.BLUE.darker());
 			offg.drawString(message, block*(xSize/3), block*(ySize/5*4));
-
-			font = new Font("Monospaced", Font.PLAIN, 12);
-			offg.setFont(font);
-			offg.setColor(Color.RED.darker());
-			offg.drawString("Left:  S(←), D(↓), E(↑), F(→)", 2*block, block*(ySize+6));
-			offg.setColor(Color.BLUE.darker());
-			offg.drawString("Right: J(←), K(↓), I(↑), L(→)", 2*block, block*(ySize+9));
 		}
-		offg.drawImage(img2, 0, block*(ySize+12), this);
+		offg.drawImage(img2, 0, block*(ySize+4), this);    // keymapの表示
 		g.drawImage(img, 0, 0, this);    // 一気に画面にコピー
 	}
 
 	public void run() {
 		Thread thisThread = Thread.currentThread();
+		// SE
 		AudioClip hit = getAudioClip(getDocumentBase(), "18am10.wav");
+		// BGM
 		AudioClip bgm = getAudioClip(getDocumentBase(), "game_maoudamashii_7_event41.mid");
 		while (thisThread==thread) {
 			initialize();
 			requestFocus();
 			if ( g_state == 1 ) {
-				bgm.loop();
+				bgm.loop();    // BGM開始
 				while (liveL&&liveR) {
-
-					xA += dxA; yA += dyA;
-
-					if (state[xA][yA]!=Color.WHITE) {
-						xA -= dxA; yA -= dyA;
-						state[xA][yA] = Color.YELLOW.darker();
+					// rand wall
+					if ( liveA == true ) {
+						xA += dxA; yA += dyA;
+						if (state[xA][yA]!=Color.WHITE) {
+							xA -= dxA; yA -= dyA;
+						}
+						state[xA][yA] = Color.BLACK;
 					}
-					state[xA][yA] = Color.BLACK;
-
+					//
 					xL += dxL; yL += dyL;
 					if (state[xL][yL]!=Color.WHITE) {
 						liveL = false;
@@ -208,18 +184,16 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 					}
 					if (!liveL) {
 						if (!liveR) {
-							g_state = 2;
+							g_state = 2;    // 引分
 							hit.play();
 							bgm.stop();
 						} else {
-							countR++;
-							g_state = 3;
+							g_state = 3;    // R勝利
 							hit.play();
 							bgm.stop();
 						}
 					} else if (!liveR) {
-						countL++;
-						g_state = 4;
+						g_state = 4;        // L勝利
 						hit.play();
 						bgm.stop();
 					}
@@ -245,12 +219,27 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 			}
 		} else {
 			Random rnd = new Random();
+			// 中心から始まるAIは、ユーザがキーをタイプする毎に動く。
 			dxA = num[rnd.nextInt(3)];
 			if(dxA == 0){
 				dyA = num[rnd.nextInt(2)];
 			}
 			else{
 				dyA = 0;
+			}
+			while (state[xA+dxA][yA+dyA]!=Color.WHITE) {
+				// 四方が白でないときAI停止
+				if ( state[xA+1][yA]!=Color.WHITE && state[xA][yA+1]!=Color.WHITE && state[xA-1][yA]!=Color.WHITE && state[xA][yA-1]!=Color.WHITE) {
+					liveA = false;
+					break;
+				}
+				dxA = num[rnd.nextInt(3)];
+				if(dxA == 0){
+					dyA = num[rnd.nextInt(2)];
+				}
+				else{
+					dyA = 0;
+				}
 			}
 			switch (key) {
 			case 'S':  if ( bKeyL == 'F' ) { break; }					// 逆向き入力の即死回避
