f9012f25653343ba18bc9c6034eb0323667ff8b7
s13t275 <dik.piyo.piyo@gmail.com>
2015-11-30 14:39:07 +0900

    edit something

--- Dron.java
+++ Dron.java
@@ -6,11 +6,11 @@ import java.awt.Image;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.Random;
-
 import javax.swing.JApplet;
 
-// bgm
-import java.applet.AudioClip;
+// 音再生
+import java.io.File;
+import javax.sound.sampled.*;
 
 public class Dron extends JApplet implements Runnable, KeyListener {
 	private Color state[][];
@@ -29,15 +29,25 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 	private int dxA, dyA;
 	private int num[] = {-1,1,0};
 
-	private Image img;     // オフスクリーンイメージ
-	private Graphics offg; // オフスクリーン用のグラフィックス
+	private Image img;      // オフスクリーンイメージ
+	private Graphics offg;  // オフスクリーン用のグラフィックス
 	private Image img2;		// キーマップイメージ
 	private int width, height;
 
-	// スタート画面表示
+	// 画面表示
+	private boolean threadSuspended = true;
 	private String title;
-	private int g_state;  // ゲームの状態 0:ゲーム開始前 1:ゲーム中 2:引き分け 3:R勝利 4:L勝利
+	private int g_state;  // ゲームの状態 0:開始前 1:ゲーム中 2:引き分け 3:R勝利 4:L勝利 5:ポーズ中
 
+	// 音再生
+	private String sound;
+	private AudioInputStream bgm_in, hit_in;
+	private Clip bgm = null, hit = null;
+	private int soundVolume = 5;    // 音量 0～10
+
+	// 難易度
+	private String difficulty;
+	private boolean normal = true;    // 難易度 true:Normal false:Hard
 
 	private void initialize() {
 		int i,j;
@@ -57,6 +67,7 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 		xA = yA = 40;
 		dxL = dxR = 0;
 		dyL = 1; dyR = -1;
+		bKeyL = 'D'; bKeyR = 'I';
 		liveL = liveR = liveA = true;
 	}
 
@@ -94,18 +105,37 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 	public void paint(Graphics g) {
 		// 全体を背景色で塗りつぶす。
 		offg.clearRect(0, 0, width, height);
+		// タイトル画面
 		if ( g_state == 0 ) {
 			// タイトル表示
 			title = "どろん";
-			font = new Font("Monospaced", Font.BOLD, 30);
+			font = new Font("Monospaced", Font.BOLD, 35);
 			offg.setFont(font);
 			offg.setColor(Color.BLACK);
-			offg.drawString(title, block*(xSize/3), block*(ySize/3));
+			offg.drawString(title, block*(xSize/3), block*(ySize/4));
+			// 難易度表示
+			message = "↑";
+			font = new Font("Monospaced", Font.BOLD, 10);
+			offg.setFont(font);
+			offg.setColor(Color.RED.darker());
+			offg.drawString(message, block*(xSize/3+12), block*(ySize/2));
+			if ( normal == true ) { difficulty = "Normal"; } else { difficulty = " Hard "; }
+			font = new Font("Monospaced", Font.BOLD, 15);
+			offg.setFont(font);
+			offg.setColor(Color.RED.darker());
+			offg.drawString(difficulty, block*(xSize/3+7), block*(ySize/2+5));
+			message = "↓";
+			font = new Font("Monospaced", Font.BOLD, 10);
+			offg.setFont(font);
+			offg.setColor(Color.RED.darker());
+			offg.drawString(message, block*(xSize/3+12), block*(ySize/2+10));
+			//
 			message = "Press the Enter";
 			font = new Font("Monospaced", Font.BOLD, 20);
 			offg.setFont(font);
 			offg.setColor(Color.RED);
 			offg.drawString(message, block*(xSize/40*9), block*(ySize/4*3));
+		// ゲーム中画面
 		} else if ( g_state == 1 ) {
 			// 一旦、別の画像（オフスクリーン）に書き込む
 			int i, j;
@@ -115,6 +145,21 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 					offg.fillRect(i*block, j*block, block, block);
 				}
 			}
+		// ポーズ画面
+		} else if ( g_state == 5 ) {
+			int i, j;
+			for (i=0; i<xSize; i++) {
+				for (j=0; j<ySize; j++) {
+					lowerAlpha(state[i][j]);    // 描画色を薄く
+					offg.fillRect(i*block, j*block, block, block);
+				}
+			}
+			message = "Pause";
+			font = new Font("Monospaced", Font.BOLD, 25);
+			offg.setFont(font);
+			offg.setColor(Color.GREEN.darker());
+			offg.drawString(message, block*(xSize/3+5), block*(ySize/2));
+		// リザルト画面
 		} else {
 			// 結果の表示
 			switch ( g_state ) {
@@ -122,50 +167,136 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 			case 3: message = "R won!"; offg.setColor(Color.BLUE.darker()); break;     // R勝利
 			case 4: message = "L won!"; offg.setColor(Color.RED.darker()); break;      // L勝利
 			}
-			font = new Font("Monospaced", Font.BOLD, 30);
+			font = new Font("Monospaced", Font.BOLD, 35);
 			offg.setFont(font);
-			offg.drawString(message, block*(xSize/3), block*(ySize/3));
+			offg.drawString(message, block*(xSize/3+2), block*(ySize/4));
 
+			// 難易度表示
+			message = "↑";
+			font = new Font("Monospaced", Font.BOLD, 10);
+			offg.setFont(font);
+			offg.setColor(Color.RED.darker());
+			offg.drawString(message, block*(xSize/3+12), block*(ySize/2));
+			if ( normal == true ) { difficulty = "Normal"; } else { difficulty = " Hard "; }
+			font = new Font("Monospaced", Font.BOLD, 15);
+			offg.setFont(font);
+			offg.setColor(Color.RED.darker());
+			offg.drawString(difficulty, block*(xSize/3+7), block*(ySize/2+5));
+			message = "↓";
+			font = new Font("Monospaced", Font.BOLD, 10);
+			offg.setFont(font);
+			offg.setColor(Color.RED.darker());
+			offg.drawString(message, block*(xSize/3+12), block*(ySize/2+10));
+			//
 			message = "Replay the game ?";
 			font = new Font("Monospaced", Font.BOLD, 17);
 			offg.setFont(font);
 			offg.setColor(Color.GREEN.darker());
-			offg.drawString(message, block*(xSize/40*9), block*(ySize/3*2));
+			offg.drawString(message, block*(xSize/40*9+2), block*(ySize/4*3));
 
 			message = "Yes: Press the Enter";
 			font = new Font("Monospaced", Font.BOLD, 12);
 			offg.setFont(font);
 			offg.setColor(Color.RED.darker());
-			offg.drawString(message, block*(xSize/3), block*(ySize/4*3));
+			offg.drawString(message, block*(xSize/3), block*(ySize/4*3+6));
 			message = "No : Close the Window";
 			offg.setColor(Color.BLUE.darker());
-			offg.drawString(message, block*(xSize/3), block*(ySize/5*4));
+			offg.drawString(message, block*(xSize/3), block*(ySize/4*3+10));
 		}
-		offg.drawImage(img2, 0, block*(ySize+4), this);    // keymapの表示
-		g.drawImage(img, 0, 0, this);    // 一気に画面にコピー
+		// 音量調整
+		sound = "Volume " + " ← " + String.format("%1$02d", soundVolume) + " → ";
+		font = new Font("Monospaced", Font.BOLD, 15);
+		offg.setFont(font);
+		offg.setColor(Color.GREEN.darker());
+		offg.drawString(sound, block*(xSize/3-1), block*(ySize+4));
+		//
+		offg.drawImage(img2, 0, block*(ySize+5), this);    // keymapの表示
+		g.drawImage(img, 0, 0, this);                      // 一気に画面にコピー
+	}
+
+	// 色のアルファ値を下げる
+	public void lowerAlpha(Color c){
+		if ( c == Color.BLACK ) {
+			offg.setColor(new Color(0, 0, 0, 60));
+		} else if ( c == Color.RED ) {
+			offg.setColor(new Color(255, 0, 0, 60));
+		} else if ( c == Color.BLUE ) {
+			offg.setColor(new Color(0, 0, 255, 60));
+		} else if ( c == Color.WHITE ) {
+			offg.setColor(c);
+		}
+	}
+
+	// 音楽ファイルの設定
+	public void setSound() {
+		try {
+			bgm = AudioSystem.getClip();
+			hit = AudioSystem.getClip();
+			bgm_in = AudioSystem.getAudioInputStream(new File("game_maoudamashii_7_event41.mid"));
+			hit_in = AudioSystem.getAudioInputStream(new File("18am10.wav"));
+			bgm.open(bgm_in);
+			hit.open(hit_in);
+			changeSoundVolume(soundVolume);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	private void controlByLinearScalar(FloatControl control, double linearScalar) {
+		control.setValue((float)Math.log10(linearScalar) * 20);
+	}
+
+	// 音量の変更
+	public void changeSoundVolume(int volume) {
+		double set_v = (double)volume / 10;
+		FloatControl bgm_control = (FloatControl)bgm.getControl(FloatControl.Type.MASTER_GAIN);
+		FloatControl hit_control = (FloatControl)hit.getControl(FloatControl.Type.MASTER_GAIN);
+		controlByLinearScalar(bgm_control, set_v);
+		controlByLinearScalar(hit_control, set_v);
 	}
 
 	public void run() {
 		Thread thisThread = Thread.currentThread();
-		// SE
-		AudioClip hit = getAudioClip(getDocumentBase(), "18am10.wav");
-		// BGM
-		AudioClip bgm = getAudioClip(getDocumentBase(), "game_maoudamashii_7_event41.mid");
 		while (thisThread==thread) {
+			setSound();
 			initialize();
 			requestFocus();
-			if ( g_state == 1 ) {
-				bgm.loop();    // BGM開始
-				while (liveL&&liveR) {
-					// rand wall
-					if ( liveA == true ) {
-						xA += dxA; yA += dyA;
-						if (state[xA][yA]!=Color.WHITE) {
-							xA -= dxA; yA -= dyA;
-						}
-						state[xA][yA] = Color.BLACK;
+			// ゲーム中でない
+			if ( g_state != 1 ) {
+				// ポーズ画面の場合bgmを停止
+				if ( g_state == 5 ) {
+					bgm.stop();
+				}
+				while ( threadSuspended ) {
+					synchronized(this) {
+						try {
+							wait();
+						} catch (InterruptedException e) {}
 					}
-					//
+				}
+			// ゲーム中
+			} else {
+				bgm.loop(-1);
+				while (liveL&&liveR) {
+					while ( threadSuspended ) {
+						synchronized(this) {
+							try {
+								wait();
+							} catch (InterruptedException e) {}
+						}
+					}
+					// 難易度がHard
+					if ( normal == false ) {
+						// rand wall
+						if ( liveA == true ) {
+							xA += dxA; yA += dyA;
+							if (state[xA][yA]!=Color.WHITE) {
+								xA -= dxA; yA -= dyA;
+							}
+							state[xA][yA] = Color.BLACK;
+						}
+						//
+					}
 					xL += dxL; yL += dyL;
 					if (state[xL][yL]!=Color.WHITE) {
 						liveL = false;
@@ -185,16 +316,19 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 					if (!liveL) {
 						if (!liveR) {
 							g_state = 2;    // 引分
-							hit.play();
+							threadSuspended = true;
+							hit.start();
 							bgm.stop();
 						} else {
 							g_state = 3;    // R勝利
-							hit.play();
+							threadSuspended = true;
+							hit.start();
 							bgm.stop();
 						}
 					} else if (!liveR) {
 						g_state = 4;        // L勝利
-						hit.play();
+						threadSuspended = true;
+						hit.start();
 						bgm.stop();
 					}
 					repaint();
@@ -205,59 +339,102 @@ public class Dron extends JApplet implements Runnable, KeyListener {
 				try{
 					Thread.sleep(1750);
 				} catch(InterruptedException e) {}
-			} else {
-
 			}
 		}
 	}
 
-	public void keyPressed(KeyEvent e) {
+	public synchronized void keyPressed(KeyEvent e) {
 		int key = e.getKeyCode();
-		if ( g_state != 1 ) {
+		// 左右キーで音量調整
+		switch ( key ) {
+		case KeyEvent.VK_LEFT: if ( soundVolume != 0 ) { changeSoundVolume(--soundVolume); repaint(); } break;
+		case KeyEvent.VK_RIGHT: if ( soundVolume != 10 ) { changeSoundVolume(++soundVolume); repaint(); } break;
+		}
+		// タイトル画面、リザルト画面でエンターキーを押すとゲーム開始、上下キーで難易度変更
+		if ( g_state != 1 && g_state != 5 ) {
+			// 難易度変更
+			if ( key == KeyEvent.VK_UP || key == KeyEvent.VK_DOWN ) {			
+				if ( normal == true ) { 
+					normal = false;
+					repaint();
+				} else {
+					normal = true;
+					repaint();
+				}
+			}
+			// ゲーム開始
 			if ( key == KeyEvent.VK_ENTER ) {
+				threadSuspended = false;
 				g_state = 1;
+				notify();
 			}
-		} else {
-			Random rnd = new Random();
-			// 中心から始まるAIは、ユーザがキーをタイプする毎に動く。
-			dxA = num[rnd.nextInt(3)];
-			if(dxA == 0){
-				dyA = num[rnd.nextInt(2)];
-			}
-			else{
-				dyA = 0;
-			}
-			while (state[xA+dxA][yA+dyA]!=Color.WHITE) {
-				// 四方が白でないときAI停止
-				if ( state[xA+1][yA]!=Color.WHITE && state[xA][yA+1]!=Color.WHITE && state[xA-1][yA]!=Color.WHITE && state[xA][yA-1]!=Color.WHITE) {
-					liveA = false;
-					break;
+		// ゲーム中
+		} else if ( g_state == 1 ) {
+			// スペースキーを押すとポーズ
+			if ( key == KeyEvent.VK_SPACE ) {
+				g_state = 5;
+				bgm.stop();
+				repaint();
+				threadSuspended = true;
+				notify();
+			} else if ( key == KeyEvent.VK_LEFT || key == KeyEvent.VK_RIGHT ) { 
+
+			} else {
+				// 難易度がHard
+				if ( normal == false ) {
+					// rand wall
+					Random rnd = new Random();
+					// 中心から始まるAIは、ユーザがキーをタイプする毎に動く。
+					dxA = num[rnd.nextInt(3)];
+					if(dxA == 0){
+						dyA = num[rnd.nextInt(2)];
+					}
+					else{
+						dyA = 0;
+					}
+					while (state[xA+dxA][yA+dyA]!=Color.WHITE) {
+						// 四方が白でないときAI停止
+						if ( state[xA+1][yA]!=Color.WHITE && state[xA][yA+1]!=Color.WHITE && state[xA-1][yA]!=Color.WHITE && state[xA][yA-1]!=Color.WHITE) {
+							liveA = false;
+							break;
+						}
+						dxA = num[rnd.nextInt(3)];
+						if(dxA == 0){
+							dyA = num[rnd.nextInt(2)];
+						}
+						else{
+							dyA = 0;
+						}
+					}
+					//
 				}
-				dxA = num[rnd.nextInt(3)];
-				if(dxA == 0){
-					dyA = num[rnd.nextInt(2)];
-				}
-				else{
-					dyA = 0;
-				}
 			}
+			// プレイヤーのキー入力
 			switch (key) {
-			case 'S':  if ( bKeyL == 'F' ) { break; }					// 逆向き入力の即死回避
+			case 'S':  if ( bKeyL == 'F' ) { break; }			// 逆向き入力の即死回避
 			else { dxL =-1; dyL = 0; bKeyL = key; break;	} 	// 1P左
 			case 'D':  if ( bKeyL == 'E' ) { break; }  
-			else { dxL = 0; dyL = 1; bKeyL = key; break; }	// 1P下
+			else { dxL = 0; dyL = 1; bKeyL = key; break; }	    // 1P下
 			case 'E':  if ( bKeyL == 'D' ) { break; }
 			else { dxL = 0; dyL =-1; bKeyL = key; break;	}	// 1P上
 			case 'F':  if ( bKeyL == 'S' ) { break; }
 			else { dxL = 1; dyL = 0; bKeyL = key; break;	}	// 1P右
 			case 'J':  if ( bKeyR == 'L' ) { break; }
-			else { dxR =-1; dyR = 0; bKeyR = key; break; }	// 2P左
+			else { dxR =-1; dyR = 0; bKeyR = key; break; }      // 2P左
 			case 'K':  if ( bKeyR == 'I' ) { break; }
-			else { dxR = 0; dyR = 1; bKeyR = key; break; }	// 2P下
+			else { dxR = 0; dyR = 1; bKeyR = key; break; }	    // 2P下
 			case 'I':  if ( bKeyR == 'K' ) { break; }
-			else { dxR = 0; dyR =-1; bKeyR = key; break; }	// 2P上
+			else { dxR = 0; dyR =-1; bKeyR = key; break; }	    // 2P上
 			case 'L':  if ( bKeyR == 'J' ) { break; }
-			else { dxR = 1; dyR = 0; bKeyR = key; break; }	// 2P右
+			else { dxR = 1; dyR = 0; bKeyR = key; break; }	    // 2P右
+			}
+		// ポーズ中にスペースキーを押すとゲーム再開
+		} else {
+			if ( key == KeyEvent.VK_SPACE ) {
+				g_state = 1;
+				bgm.loop(-1);
+				threadSuspended = false;
+				notify();
 			}
 		}
 	}
